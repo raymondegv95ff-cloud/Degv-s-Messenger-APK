bd418fe274a5964de49a6beeca94b4551e80c3b4
Ramon Eugenio Gonzalez Vargas <raymondegv95ff@gmail.com>
2026-08-16 00:23:43 -0400

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-[#050505] text-[#00E676] font-sans">
+      <h1 className="text-3xl font-bold">Degv's Messenger</h1>
+    </div>
+  )
+}
